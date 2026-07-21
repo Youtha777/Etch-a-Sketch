759c5a671eeab5161157f78fdf95a0fda9f70aa9
Youtharin Kuy <61557913+Youtha777@users.noreply.github.com>
2026-07-21 15:39:32 -0700

    Updated javascript code to include 256 squares (16x16) and updated css files to include 16x16 grid and center it using flexbox.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,7 @@
 
 const container = document.querySelector("#container");
 
-for (let  i = 0; i < 16; i++){
+for (let  i = 0; i < 256; i++){
   const square = document.createElement('div');
 
   square.classList.add("square");
