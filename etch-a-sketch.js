523e736ab4ac524b3b9c3c0c391b942963069d4f
Youtharin Kuy <61557913+Youtha777@users.noreply.github.com>
2026-07-22 14:38:36 -0700

    Add event listener when mouse is hover over a square and to change the backgroud color of the square - in javascript file.  Add a button and an input (html) for user to enter the amount of squares they want per side - which generate a new canvas when the button is clicked (event - in javascript).

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,62 @@
 
 const container = document.querySelector("#container");
+const squares = document.querySelectorAll(".square");
+const generateBtn = document.querySelector("#generateCanvas");
+const numOfSquares = document.querySelector("#canvasNum");
 
-for (let  i = 0; i < 256; i++){
+const containerSize = 800;
+container.style.width = `${containerSize}px`;
+container.style.height = `${containerSize}px`;
+
+generateCanvas(8,containerSize/8 );
+
+generateBtn.addEventListener("click", clicked);
+
+function generateCanvas(num, squareSize = 50){
+
+  num = num*num;
+
+  for (let  i = 0; i < num; i++){
   const square = document.createElement('div');
-
   square.classList.add("square");
+  square.id = i;
+  
+  
+  square.style.width = `${squareSize}px`;
+  square.style.height = `${squareSize}px`;
 
-  container.appendChild(square);
+
+    square.addEventListener("mouseenter", () => {
+      const bg = square.style.backgroundColor;
+      if (bg === ""){
+        square.style.backgroundColor = randomRGB();
+      }
+      
+    });
+
+    container.appendChild(square);
+
+  }
+
+}
+
+function clicked (){
+  const squaresPerSide = parseInt(numOfSquares.value);
+
+  container.innerHTML = "";
+
+  const squareSize = containerSize/squaresPerSide;
+
+  generateCanvas(squaresPerSide, squareSize);
+
+}
+
+function randomRGB (){
+
+   // Generate random values between 0 and 255
+   const r = Math.floor(Math.random() * 256);
+   const g = Math.floor(Math.random() * 256);
+   const b = Math.floor(Math.random() * 256);
+  
+   return `rgb(${r}, ${g}, ${b})`
 }
